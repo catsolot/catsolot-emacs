16e7f905619e6f701cb09e66c8a2b03de9389828
milo craun <milo@ipsum.localdomain>
2022-04-08 14:38:37 -0400

    added install and README

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln --interactive --symbolic $HOME/.config/catsolot-emacs/emacs-profiles.el $HOME/.emacs-profiles.el 
+ln --interactive --symbolic $HOME/.config/catsolot-emacs/emacs-profile $HOME/.emacs-profile
